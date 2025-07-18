2df540bb00a263f1922952b2bcab81518c966d80
Eryk Fernandes <erykfernandespro@gmail.com>
2025-07-18 15:06:30 -0300

    Feat: inserir usuários

--- src/main/java/com/mycompany/sistema_academico/controller/UserController.java
+++ src/main/java/com/mycompany/sistema_academico/controller/UserController.java
@@ -1,13 +1,13 @@
 package com.mycompany.sistema_academico.controller;
 
+import com.mycompany.sistema_academico.dto.UserCreateDTO;
 import com.mycompany.sistema_academico.dto.UserResponseDTO;
 import com.mycompany.sistema_academico.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/users")
@@ -25,7 +25,13 @@ public class UserController {
             return ResponseEntity.ok(userResponseDTO);
         }
         return ResponseEntity.notFound().build();
+    }
 
+    @PostMapping()
+    public ResponseEntity<UserResponseDTO> createUser(@RequestBody UserCreateDTO userCreateDTO) {
+        UserResponseDTO userResponseDTO = userService.createUser(userCreateDTO);
+
+        return ResponseEntity.created(URI.create("users/" + userResponseDTO.userId())).build();
     }
 
 }
--- src/main/java/com/mycompany/sistema_academico/service/UserService.java
+++ src/main/java/com/mycompany/sistema_academico/service/UserService.java
@@ -1,11 +1,12 @@
 package com.mycompany.sistema_academico.service;
 
+import com.mycompany.sistema_academico.dto.UserCreateDTO;
 import com.mycompany.sistema_academico.dto.UserResponseDTO;
 import com.mycompany.sistema_academico.entity.User;
 import com.mycompany.sistema_academico.mapper.UserMapper;
 import com.mycompany.sistema_academico.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,6 +16,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class UserService {
 
+    private final PasswordEncoder passwordEncoder;
     private UserRepository userRepository;
     private UserMapper userMapper;
 
@@ -25,7 +27,17 @@ public class UserService {
             return userMapper.toDTO(user.get());
         }
         return null;
+    }
 
+    public UserResponseDTO createUser(UserCreateDTO userCreateDTO) {
+        User user = new User();
+        user.setEmail(userCreateDTO.email());
+        user.setPassword(passwordEncoder.encode(userCreateDTO.password()));
+        user.setUsername(userCreateDTO.username());
+
+        User savedUser = userRepository.save(user);
+
+        return new UserResponseDTO(savedUser.getUserId(), savedUser.getUsername(), savedUser.getEmail());
     }
 
 }
